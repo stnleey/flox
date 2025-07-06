6acd5fb7f0bd5d3dfc37ac323cfda4d9df80721d
eeiaao <eeiaao@gmail.com>
2025-07-06 09:12:39 +0300

    Update copyrights (#22)

--- include/flox/book/events/trade_event.h
+++ include/flox/book/events/trade_event.h
@@ -1,3 +1,12 @@
+/*
+ * Flox Engine
+ * Developed by Evgenii Makarov (https://github.com/eeiaao)
+ *
+ * Copyright (c) 2025 Evgenii Makarov
+ * Licensed under the MIT License. See LICENSE file in the project root for full
+ * license information.
+ */
+
 #pragma once
 
 #include "flox/book/trade.h"
--- tests/test_decimal.cpp
+++ tests/test_decimal.cpp
@@ -1,3 +1,12 @@
+/*
+ * Flox Engine
+ * Developed by Evgenii Makarov (https://github.com/eeiaao)
+ *
+ * Copyright (c) 2025 Evgenii Makarov
+ * Licensed under the MIT License. See LICENSE file in the project root for full
+ * license information.
+ */
+
 #include "flox/util/base/decimal.h"
 
 #include <gtest/gtest.h>
