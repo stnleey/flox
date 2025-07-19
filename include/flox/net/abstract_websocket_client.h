51da56908dbf3dfbc762361714baea40e6479e21
eeiaao <eeiaao@gmail.com>
2025-07-19 11:05:04 +0300

    Add IWebSocketClient (#28)

---
+++ include/flox/net/abstract_websocket_client.h
@@ -0,0 +1,28 @@
+/*
+ * Flox Engine
+ * Developed by FLOX Foundation (https://github.com/FLOX-Foundation)
+ *
+ * Copyright (c) 2025 FLOX Foundation
+ * Licensed under the MIT License. See LICENSE file in the project root for full
+ * license information.
+ */
+
+#pragma once
+
+#include <functional>
+#include <string>
+#include <string_view>
+
+class IWebSocketClient
+{
+ public:
+  virtual ~IWebSocketClient() = default;
+
+  virtual void onOpen(std::move_only_function<void()> cb) = 0;
+  virtual void onMessage(std::move_only_function<void(std::string_view)> cb) = 0;
+  virtual void onClose(std::move_only_function<void(int, std::string_view)> cb) = 0;
+
+  virtual void send(const std::string& data) = 0;
+  virtual void start() = 0;
+  virtual void stop() = 0;
+};
